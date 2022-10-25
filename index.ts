c5f8445a23a35997fbb816e14b3ee9f343dfe0d9
bilmans <bilal.manssouri@gmail.com>
2022-10-25 15:04:32 +0200

    plus joli

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import { serve } from "https://deno.land/std@0.119.0/http/server.ts";
 async function handler(_req: Request): Promise<Response> {
   const guess = await extractGuess(_req)
   const number = await similarity(guess)
+  const stringResponse = "The word " + guess + "has a score of" + number
   return new Response(JSON.stringify(number));
 }
 
